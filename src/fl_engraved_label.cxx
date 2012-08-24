61218d3092d3b75bccd3a10ef47412d21e5ad0a1
Lauri Kasanen <cand@gmx.com>
2012-08-24 19:16:15 +0300

    Constify engraved label data, move 192 bytes to RO

--- src/fl_engraved_label.cxx
+++ src/fl_engraved_label.cxx
@@ -35,7 +35,7 @@
 
 static void innards(
     const Fl_Label* o, int X, int Y, int W, int H, Fl_Align align,
-    int data[][3], int n)
+    const int data[][3], int n)
 {
   Fl_Align a1 = align;
   if (a1 & FL_ALIGN_CLIP) {
@@ -51,14 +51,14 @@ static void innards(
 static void fl_shadow_label(
     const Fl_Label* o, int X, int Y, int W, int H, Fl_Align align)
 {
-  static int data[2][3] = {{2,2,FL_DARK3},{0,0,0}};
+  static const int data[2][3] = {{2,2,FL_DARK3},{0,0,0}};
   innards(o, X, Y, W, H, align, data, 2);
 }
 
 static void fl_engraved_label(
     const Fl_Label* o, int X, int Y, int W, int H, Fl_Align align)
 {
-  static int data[7][3] = {
+  static const int data[7][3] = {
     {1,0,FL_LIGHT3},{1,1,FL_LIGHT3},{0,1,FL_LIGHT3},
     {-1,0,FL_DARK3},{-1,-1,FL_DARK3},{0,-1,FL_DARK3},
     {0,0,0}};
@@ -68,7 +68,7 @@ static void fl_engraved_label(
 static void fl_embossed_label(
     const Fl_Label* o, int X, int Y, int W, int H, Fl_Align align)
 {
-  static int data[7][3] = {
+  static const int data[7][3] = {
     {-1,0,FL_LIGHT3},{-1,-1,FL_LIGHT3},{0,-1,FL_LIGHT3},
     {1,0,FL_DARK3},{1,1,FL_DARK3},{0,1,FL_DARK3},
     {0,0,0}};
