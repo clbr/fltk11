83fbe8ffbb93fbbeafd4547ced99cf790c5d9dec
Lauri Kasanen <cand@gmx.com>
2012-12-09 11:49:07 +0200

    Enable symbols in build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 export LDFLAGS="-Wl,-O1 -Wl,-gc-sections -Wl,-as-needed"
-export CFLAGS="-ffunction-sections -fdata-sections -Os -s -pipe -fomit-frame-pointer"
+export CFLAGS="-ffunction-sections -fdata-sections -Os -pipe -fomit-frame-pointer"
 export CXXFLAGS="$CFLAGS -fno-rtti -fno-exceptions"
 
 ./configure --disable-gl --enable-shared
